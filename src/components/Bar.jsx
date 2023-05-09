bf5f47a83902efc038a532a8fd4a31dbb858c0e9
Viktor Hajdu <vi.h.seo@gmail.com>
2023-05-09 22:54:11 +0200

    create enums an style for bar

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,5 +1,53 @@
+import React from "react";
+
+const modes = [
+  { id: 0, name: "Start" },
+  { id: 1, name: "End" },
+  { id: 2, name: "Wall" },
+  { id: 3, name: "Clear" },
+];
+const algorithms = [
+  { id: 0, name: "Dijkstra" },
+  { id: 1, name: "A*" },
+];
+
 const Bar = () => {
   return <div>bar</div>;
 };
 
+const styles = {
+  main: {
+    width: "100%",
+    color: "white",
+    height: "fit-content",
+    margin: "0px auto",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#bbdefb",
+    fontWeight: "bold",
+    width: "80px",
+    color: "black",
+    border: "none",
+    padding: "10px",
+    borderRadius: "5px",
+    margin: "5px 5px",
+    cursor: "pointer",
+  },
+  disabledText: {
+    color: "#fff",
+    height: "15px",
+  },
+  buttonBackground: {
+    backgroundColor: "#fff",
+    borderRadius: "5px",
+    padding: "3px",
+    margin: "5px",
+    justifyContent: "center",
+    display: "flex",
+    flexWrap: "wrap",
+  },
+};
 export default Bar;
